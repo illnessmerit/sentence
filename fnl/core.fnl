0dd451c213826aeb1039005f1a79e23ebedcd99d
a <a@as-Virtual-Machine.local>
2026-03-25 21:37:26 -0700

    Replace core-prefixed function calls

--- fnl/core.fnl
+++ fnl/core.fnl
@@ -1,24 +1,23 @@
-(local core (require :nfnl.core))
+(local {: concat : empty? : first : identity : inc : last : map &as core}
+       (require :nfnl.core))
 
 (fn find-all* [s pattern hits]
   (let [hit [(string.find s pattern)]]
-    (if (core.empty? hit)
+    (if (empty? hit)
         hits
         (find-all* (->> hit
-                        core.first
-                        core.inc
+                        first
+                        inc
                         (string.sub s)) pattern
-                   (core.concat hits
-                                [(core.map (if (core.empty? hits)
-                                               core.identity
-                                               (partial +
-                                                        (core.first (core.last hits))))
-                                           hit)])))))
+                   (concat hits [(map (if (empty? hits)
+                                          identity
+                                          (partial + (first (last hits))))
+                                      hit)])))))
 
 (fn find-all [s pattern]
   (find-all* s pattern []))
 
 (fn find-line-end [line]
-  (core.first [(string.find line "%S%s*$")]))
+  (first [(string.find line "%S%s*$")]))
 
 {}
--- lua/core.lua
+++ lua/core.lua
@@ -1,27 +1,35 @@
 -- [nfnl] fnl/core.fnl
-local core = require("nfnl.core")
+local _local_1_ = require("nfnl.core")
+local concat = _local_1_.concat
+local empty_3f = _local_1_["empty?"]
+local first = _local_1_.first
+local identity = _local_1_.identity
+local inc = _local_1_.inc
+local last = _local_1_.last
+local map = _local_1_.map
+local core = _local_1_
 local function find_all_2a(s, pattern, hits)
   local hit = {string.find(s, pattern)}
-  if core["empty?"](hit) then
+  if empty_3f(hit) then
     return hits
   else
-    local _1_
-    if core["empty?"](hits) then
-      _1_ = core.identity
+    local _2_
+    if empty_3f(hits) then
+      _2_ = identity
     else
-      local partial_2_ = core.first(core.last(hits))
-      local function _4_(...)
-        return (partial_2_ + ...)
+      local partial_3_ = first(last(hits))
+      local function _5_(...)
+        return (partial_3_ + ...)
       end
-      _1_ = _4_
+      _2_ = _5_
     end
-    return find_all_2a(string.sub(s, core.inc(core.first(hit))), pattern, core.concat(hits, {core.map(_1_, hit)}))
+    return find_all_2a(string.sub(s, inc(first(hit))), pattern, concat(hits, {map(_2_, hit)}))
   end
 end
 local function find_all(s, pattern)
   return find_all_2a(s, pattern, {})
 end
 local function find_line_end(line)
-  return core.first({string.find(line, "%S%s*$")})
+  return first({string.find(line, "%S%s*$")})
 end
 return {}
