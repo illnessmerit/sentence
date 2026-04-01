160e016c656df5f3eb9ca634a8a60d415abfce26
a <a@as-Virtual-Machine.local>
2026-04-01 02:16:44 -0700

    Add find-list-item-ends for detecting numbered list prefixes in a line

--- src/main.cljs
+++ src/main.cljs
@@ -8,15 +8,15 @@
   (concat xs [x]))
 
 (defn find-all*
-  [s pattern hits]
-  (let [hit (.exec pattern s)]
+  [s query hits]
+  (let [hit (.exec query s)]
     (if hit
-      (find-all* s pattern (snoc hits [(.-index hit) (.-lastIndex pattern)]))
+      (find-all* s query (snoc hits [(.-index hit) (.-lastIndex query)]))
       hits)))
 
 (defn find-all
-  [s pattern]
-  (find-all* s (js/RegExp. pattern "g") []))
+  [s re]
+  (find-all* s (js/RegExp. re "g") []))
 
 (defn find-line-end
   [line]
@@ -38,6 +38,14 @@
                                         last))
                      (partial find-all line))
                honorifics)))
+
+(defn find-list-item-ends
+  [line]
+  (let [query (js/RegExp. #"^\s*\d+\." "g")
+        hit (.exec query line)]
+    (if hit
+      #{(dec (.-lastIndex query))}
+      #{})))
 
 (defn main
   [plugin]
